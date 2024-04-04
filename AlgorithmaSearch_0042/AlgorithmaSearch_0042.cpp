63cba574a781e1c58dc879949c8125f66dbcb697
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 19:28:57 +0700

    input 2

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -21,6 +21,7 @@ void input()
 	cout << "\n-----------------\n";
 	cout << " enter array elements \n";
 	cout << "----------------------\n";
+
 	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << "> ";
