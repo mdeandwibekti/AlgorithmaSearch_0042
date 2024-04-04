35b9995cf7db7de3ad24d46fe733b5eb1813fa8e
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 19:17:33 +0700

    void linearsearch 4

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -46,7 +46,12 @@ void linearsearch()
 		i = 0; //step 2
 		while (i < n) //step 3
 		{
-
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
+				break;
+			}
 		}
 	}
 
