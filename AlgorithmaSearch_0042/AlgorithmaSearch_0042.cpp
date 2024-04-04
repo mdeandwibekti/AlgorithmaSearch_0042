af9e5950378ccfd0336698dde9779d6e4940cc2a
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 19:29:23 +0700

    selesai

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -21,7 +21,6 @@ void input()
 	cout << "\n-----------------\n";
 	cout << " enter array elements \n";
 	cout << "----------------------\n";
-
 	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << "> ";
