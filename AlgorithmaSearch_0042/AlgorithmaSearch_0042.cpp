30a5ed42f1794ce36e9249c2f92d1512a96e3bec
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 19:15:56 +0700

    voidlinearseacrh 3

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -41,7 +41,17 @@ void linearsearch()
 		//Accept the number to be searched
 		cout << "\nEnter the element you want to search : "; //step 1
 		cin >> item;
+
+		ctr = 0;
+		i = 0; //step 2
+		while (i < n) //step 3
+		{
+
+		}
 	}
+
+
+
 }
 
 
