548986ea1aa253c7f891b13666ec6b7e3ad7f1b9
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 18:54:09 +0700

    memasukan wlhie

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
-int i;
+int arr[20]; //Array to be searched
+int n;       //Number of elements in the array
+int i;       //Index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
+	}
+}
+
+
+
