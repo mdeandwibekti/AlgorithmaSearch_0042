cea5446b5431ea7292a3192fcf44c5fb432be59f
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-04 19:22:51 +0700

    void linearsearch 7

--- AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
+++ AlgorithmaSearch_0042/AlgorithmaSearch_0042.cpp
@@ -59,6 +59,7 @@ void linearsearch()
 			cout << "\n" << item << " not found in the array\n";
 		cout << "\nNumber of comparisons : " << ctr << endl;
 
+
 		cout << "\nContinue search (y/n) : ";
 		cin >> ch;
 
